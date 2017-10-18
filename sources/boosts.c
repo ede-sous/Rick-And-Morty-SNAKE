3b77a3ee27e68a343ea070b78177d4cd6eea2529
ede-sous <ede-sous@student.42.fr>
2017-10-18 14:26:13 +0200

    changes3

--- sources/boosts.c
+++ sources/boosts.c
@@ -14,141 +14,140 @@
 
 void                Boost_Speed(int value, t_fun *fun)
 {
-    static int      boost = 200;
-    static int      what = 0;
+	static int      boost = 200;
+	static int      what = 0;
 
-    if (value > 0 && what == 0)
-    {
-        Expand_One(fun);
-        system("killall sh | killall afplay");
-        system(value == 1 ? "afplay .sound/speed_boost.mp3 &>/dev/null &"
-                            : "afplay .sound/slow_speed.mp3 &>/dev/null &");
-    }
-    if (!fun)
-    {
-        boost = 200;
-        what = 0;
-        return ;
-    }
-    if (what == 0 && value && boost >= 200)
-    {
-        boost = 0;
-        what = value;
-    }
-    else if (what == 1 && boost < 400)
-        GIVE_Delay = 0;
-    else if (what == 2 && boost < 200)
-        GIVE_Delay = 120;
-    else
-        GIVE_Delay = 50;
-    if ((boost >= 200 && what == 2) || (boost >= 400 && what == 1))
-    {
-        system("killall afplay");
-        system ("while :;do afplay .sound/game_music.mp3; done &>/dev/null &");
-        what = 0;
-    }
-    else
-        boost++;
+	if (value > 0 && what == 0)
+	{
+		Expand_One(fun);
+		system("killall sh | killall afplay");
+		system(value == 1 ? "afplay .sound/speed_boost.mp3 &>/dev/null &"
+				: "afplay .sound/slow_speed.mp3 &>/dev/null &");
+	}
+	if (!fun)
+	{
+		boost = 200;
+		what = 0;
+		return ;
+	}
+	if (what == 0 && value && boost >= 200)
+	{
+		boost = 0;
+		what = value;
+	}
+	else if (what == 1 && boost < 400)
+		GIVE_Delay = 0;
+	else if (what == 2 && boost < 200)
+		GIVE_Delay = 120;
+	else
+		GIVE_Delay = 50;
+	if ((boost >= 200 && what == 2) || (boost >= 400 && what == 1))
+	{
+		system("killall afplay");
+		system ("while :;do afplay .sound/game_music.mp3; done &>/dev/null &");
+		what = 0;
+	}
+	else
+		boost++;
 }
 
 static void         Expand_More(t_fun *fun)
 {
-    int             i = 0;
+	int             i = 0;
 
-    if (Mouses_Eaten(2) < 4)
-    {
-        Expand_One(fun);
-        return ;
-    }
-    system("afplay .sound/expand_more.mp3 &>/dev/null &");
-    while (++i < 4)
-        Expand_Snake(fun);
+	if (Mouses_Eaten(2) < 4)
+	{
+		Expand_One(fun);
+		return ;
+	}
+	system("afplay .sound/expand_more.mp3 &>/dev/null &");
+	while (++i < 4)
+		Expand_Snake(fun);
 }
 
 static void         Take_Half_Body(t_fun *fun)
 {
-    int             suffer = Mouses_Eaten(2), i = 0;
+	int             suffer = Mouses_Eaten(2), i = 0;
 
-    if (suffer < 4)
-    {
-        Expand_One(fun);
-        return ;
-    }
-    system("afplay .sound/half_out.mp3 &>/dev/null &");
-    Mouses_Eaten(0);
-    suffer = (suffer / 2) + (suffer % 2);
-    FBodyTmp = FBody;
-    while (++i <= suffer)
-    {
-        Mouses_Eaten(1);
-        FBodyTmp = FBodyTmp->next;
-    }
-    FBodyTmp = FBodyTmp->prev;
-    FBodyTmp->next = NULL;
+	if (suffer < 4)
+	{
+		Expand_One(fun);
+		return ;
+	}
+	system("afplay .sound/half_out.mp3 &>/dev/null &");
+	Mouses_Eaten(0);
+	suffer = (suffer / 2) + (suffer % 2);
+	FBodyTmp = FBody;
+	while (++i <= suffer)
+	{
+		Mouses_Eaten(1);
+		FBodyTmp = FBodyTmp->next;
+	}
+	FBodyTmp = FBodyTmp->prev;
+	FBodyTmp->next = NULL;
 }
 
-static void         Expand_Half(t_fun *fun)
+static void			Expand_Half(t_fun *fun)
 {
-    int             yey = Mouses_Eaten(2), i = 0;
+	int				yey = Mouses_Eaten(2), i = 0;
 
-    if (Fhs.highscore && (yey <= 1 || yey > (Fhs.highscore / 2)))
-    {
-        Expand_One(fun);
-        return ;
-    }
-    yey = (yey / 2) + (yey % 2);
-    system("afplay .sound/expand_half.mp3 &>/dev/null &");
-    while (++i <= yey)
-        Expand_Snake(fun);
-    FBodyTmp->next = NULL;
+	if (Fhs.highscore && (yey <= 1 || yey > (Fhs.highscore / 2)))
+	{
+		Expand_One(fun);
+		return ;
+	}
+	system("afplay .sound/expand_half.mp3 &>/dev/null &");
+	while (++i <= yey)
+		Expand_Snake(fun);
+	FBodyTmp->next = NULL;
 }
 
 
 static void         Take_One_Out(t_fun *fun)
 {
-    int             suffer = Mouses_Eaten(2), i = 0;
+	int             suffer = Mouses_Eaten(2), i = 0;
 
-    if (suffer < 4)
-    {
-        Expand_One(fun);
-        return ;
-    }
-    Mouses_Eaten(0);
-    suffer -= 1;
-    FBodyTmp = FBody;
-    system("afplay .sound/one_out.mp3 &>/dev/null &");
-    while (++i <= suffer)
-    {
-        Mouses_Eaten(1);
-        FBodyTmp = FBodyTmp->next;
-    }
-    FBodyTmp = FBodyTmp->prev;
-    FBodyTmp->next = NULL;
+	if (suffer < 4)
+	{
+		Expand_One(fun);
+		return ;
+	}
+	Mouses_Eaten(0);
+	suffer -= 1;
+	FBodyTmp = FBody;
+	system("afplay .sound/one_out.mp3 &>/dev/null &");
+	while (++i <= suffer)
+	{
+		Mouses_Eaten(1);
+		FBodyTmp = FBodyTmp->next;
+	}
+	FBodyTmp = FBodyTmp->prev;
+	FBodyTmp->next = NULL;
 }
 
 void               Expand_One(t_fun *fun)
 {
-    system("afplay .sound/turtle.mp3 &>/dev/null &");
-    Expand_Snake(fun);
+	system("afplay .sound/turtle.mp3 &>/dev/null &");
+	Expand_Snake(fun);
 }
 
 void                Random_Boost(t_fun *fun)
 {
-    int             random;
+	int             random;
 
-    random = (rand() % 26);
-    if (random == 25)
-        Boost_Speed(1, fun);
-    else if (random == 16)
-        Boost_Speed(2, fun);
-    else if (random == 22)
-        Expand_More(fun);
-    else if (random == 9)
-        Take_Half_Body(fun);
-    else if (random == 10)
-        Take_One_Out(fun);
-    else if (random == 8)
-        Expand_Half(fun);
-    else
-        Expand_One(fun);
+	random = (rand() % 26);
+	if (random == 25)
+		Boost_Speed(1, fun);
+	else if (random == 16)
+		Boost_Speed(2, fun);
+	else if (random == 22)
+		Expand_More(fun);
+	else if (random == 9)
+		Take_Half_Body(fun);
+	else if (random == 10)
+		Take_One_Out(fun);
+	else if (random == 8)
+		Expand_Half(fun);
+	else
+		Expand_One(fun);
 }
